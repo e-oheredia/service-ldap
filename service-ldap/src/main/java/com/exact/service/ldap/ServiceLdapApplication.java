3be622bce6d019027fb3dd942596975551c82fe0
e-oheredia <oheredia@exact.com.pe>
2019-10-02 10:25:54 -0500

    Cors

--- service-ldap/src/main/java/com/exact/service/ldap/ServiceLdapApplication.java
+++ service-ldap/src/main/java/com/exact/service/ldap/ServiceLdapApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServiceLdapApplication extends SpringBootServletInitializer {
@@ -16,5 +19,18 @@ public class ServiceLdapApplication extends SpringBootServletInitializer {
 	 protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
 	         return app.sources(ServiceLdapApplication.class);
 	     }
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/ldap").allowedOrigins("http://192.168.1.224:8087")
+                .allowCredentials(true)
+                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS")
+                .allowedHeaders("Content-Type, Authorization, Data-Type");
+            }
+        };
+    }
 
 }
